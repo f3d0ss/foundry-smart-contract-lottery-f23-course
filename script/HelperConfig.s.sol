dad59ae319d41fc48a2f77df26ab0fadce5e9d0d
f3d0ss <federico.cazzola@mail.polimi.it>
2023-06-30 16:02:49 +0200

    feat: project completed

---
+++ script/HelperConfig.s.sol
@@ -0,0 +1,75 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.19;
+
+import {Script} from "forge-std/Script.sol";
+import {VRFCoordinatorV2Mock} from "@chainlink/contracts/src/v0.8/mocks/VRFCoordinatorV2Mock.sol";
+import {LinkToken} from "../test/mocks/LinkToken.sol";
+
+contract HelperConfig is Script {
+    struct NetworkConfig {
+        uint256 entranceFee;
+        uint256 interval;
+        address vrfCoordinator;
+        bytes32 gasLane;
+        uint64 subscriptionId;
+        uint32 callbackGasLimit;
+        address link;
+        uint256 deployerKey;
+    }
+
+    NetworkConfig public activeNetworkConfig;
+
+    constructor() {
+        if (block.chainid == 11155111) {
+            activeNetworkConfig = getSepoliaEthConfig();
+        } else {
+            activeNetworkConfig = getOrCreateAnvilEthConfig();
+        }
+    }
+
+    function getSepoliaEthConfig() public returns (NetworkConfig memory) {
+        NetworkConfig memory sepoliaConfig = NetworkConfig({
+            entranceFee: 0.01 ether,
+            interval: 30,
+            vrfCoordinator: 0x8103B0A8A00be2DDC778e6e7eaa21791Cd364625,
+            gasLane: 0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c,
+            subscriptionId: uint64(vm.envOr("VRF_SUB_ID", uint256(0))),
+            callbackGasLimit: 500000,
+            link: 0x779877A7B0D9E8603169DdbD7836e478b4624789,
+            deployerKey: vm.envUint("SEPOLIA_PRIVATE_KEY")
+        });
+
+        return sepoliaConfig;
+    }
+
+    function getOrCreateAnvilEthConfig() public returns (NetworkConfig memory) {
+        if (activeNetworkConfig.vrfCoordinator != address(0)) {
+            return activeNetworkConfig;
+        }
+
+        uint96 baseFee = 0.25 ether; // 0.25 LINK
+        uint96 gasPriceLink = 1e9; // 1 gwei Link
+
+        // Mock VRF
+        vm.startBroadcast();
+        VRFCoordinatorV2Mock vrfCoordinator = new VRFCoordinatorV2Mock(
+            baseFee,
+            gasPriceLink
+        );
+        LinkToken link = new LinkToken();
+        vm.stopBroadcast();
+
+        NetworkConfig memory anvilConfig = NetworkConfig({
+            entranceFee: 0.01 ether,
+            interval: 30,
+            vrfCoordinator: address(vrfCoordinator),
+            gasLane: 0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c,
+            subscriptionId: 0, // TODO: Update this with our subId!
+            callbackGasLimit: 500000,
+            link: address(link),
+            deployerKey: vm.envUint("ANVIL_PRIVATE_KEY")
+        });
+
+        return anvilConfig;
+    }
+}
---
+++ test/integration/InteractionsTest.t.sol
@@ -0,0 +1,4 @@
+// unit
+// integration (deploy and other components)
+// forked
+// staging <- run tests on a mainnet/testnet
